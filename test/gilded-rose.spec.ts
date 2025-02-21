5aaebabd89da4db03e278d86346081b055dfc5be
Sander Maas <sandermaas7@hotmail.com>
2025-02-21 21:00:05 +0100

    Created negative quality test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -28,4 +28,18 @@ describe('Gilded Rose', () => {
 		expect(barItem.quality).toBe(3);
 		expect(barItem.sellIn).toBe(-3);
 	});
+
+	it('should not decrease the quality of an item below 0', () => {
+		const fooItem = new Item('foo', 20, 0);
+		const barItem = new Item('bar', -4, 0);
+
+		const gildedRose = new GildedRose([fooItem, barItem]);
+
+		gildedRose.updateQuality();
+
+		expect(fooItem.quality).toBe(0);
+		expect(fooItem.sellIn).toBe(19);
+		expect(barItem.quality).toBe(0);
+		expect(barItem.sellIn).toBe(-5);
+	});
 });
