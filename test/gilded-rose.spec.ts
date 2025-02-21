b42757d90e9ec821618e4b90ef3426db457d4b1a
Sander Maas <sandermaas7@hotmail.com>
2025-02-21 20:54:27 +0100

    Created decrease quality/sellin test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,9 +1,17 @@
 import { GildedRose, Item } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-	it('should foo', () => {
-		const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-		const items = gildedRose.updateQuality();
-		expect(items[0].name).toBe('fixme');
+	it('should decrease the quality and sellin of an item by 1', () => {
+		const fooItem = new Item('foo', 2, 10);
+		const barItem = new Item('bar', 4, 5);
+
+		const gildedRose = new GildedRose([fooItem, barItem]);
+
+		gildedRose.updateQuality();
+
+		expect(fooItem.quality).toBe(9);
+		expect(fooItem.sellIn).toBe(1);
+		expect(barItem.quality).toBe(4);
+		expect(barItem.sellIn).toBe(3);
 	});
 });
