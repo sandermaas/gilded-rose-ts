6eb1d36b60e090fe8a83ce9801429fcaab758677
Sander Maas <sandermaas7@hotmail.com>
2025-02-21 20:56:21 +0100

    Created double degradation test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -14,4 +14,18 @@ describe('Gilded Rose', () => {
 		expect(barItem.quality).toBe(4);
 		expect(barItem.sellIn).toBe(3);
 	});
+
+	it('should decrease the quality of an item by 2 when sellIn is 0 or less', () => {
+		const fooItem = new Item('foo', 0, 10);
+		const barItem = new Item('bar', -2, 5);
+
+		const gildedRose = new GildedRose([fooItem, barItem]);
+
+		gildedRose.updateQuality();
+
+		expect(fooItem.quality).toBe(8);
+		expect(fooItem.sellIn).toBe(-1);
+		expect(barItem.quality).toBe(3);
+		expect(barItem.sellIn).toBe(-3);
+	});
 });
