c083677dcb4effd193d911007ef978a1647faf6c
Sander Maas <sandermaas7@hotmail.com>
2025-02-21 21:45:10 +0100

    Moved update logic of items to Item class

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,4 +1,3 @@
-import { AGED_BRIE, BACKSTAGE_PASSES, SULFURAS } from '@/constants';
 import { Item } from '@/item';
 
 export class GildedRose {
@@ -9,55 +8,7 @@ export class GildedRose {
 	}
 
 	updateQuality() {
-		for (let i = 0; i < this.items.length; i++) {
-			if (
-				this.items[i].name != AGED_BRIE &&
-				this.items[i].name != BACKSTAGE_PASSES
-			) {
-				if (this.items[i].quality > 0) {
-					if (this.items[i].name != SULFURAS) {
-						this.items[i].quality = this.items[i].quality - 1;
-					}
-				}
-			} else {
-				if (this.items[i].quality < 50) {
-					this.items[i].quality = this.items[i].quality + 1;
-					if (this.items[i].name == BACKSTAGE_PASSES) {
-						if (this.items[i].sellIn < 11) {
-							if (this.items[i].quality < 50) {
-								this.items[i].quality = this.items[i].quality + 1;
-							}
-						}
-						if (this.items[i].sellIn < 6) {
-							if (this.items[i].quality < 50) {
-								this.items[i].quality = this.items[i].quality + 1;
-							}
-						}
-					}
-				}
-			}
-			if (this.items[i].name != SULFURAS) {
-				this.items[i].sellIn = this.items[i].sellIn - 1;
-			}
-			if (this.items[i].sellIn < 0) {
-				if (this.items[i].name != AGED_BRIE) {
-					if (this.items[i].name != BACKSTAGE_PASSES) {
-						if (this.items[i].quality > 0) {
-							if (this.items[i].name != SULFURAS) {
-								this.items[i].quality = this.items[i].quality - 1;
-							}
-						}
-					} else {
-						this.items[i].quality =
-							this.items[i].quality - this.items[i].quality;
-					}
-				} else {
-					if (this.items[i].quality < 50) {
-						this.items[i].quality = this.items[i].quality + 1;
-					}
-				}
-			}
-		}
+		this.items.forEach((item) => item.update());
 
 		return this.items;
 	}
--- app/item.ts
+++ app/item.ts
@@ -1,3 +1,5 @@
+import { AGED_BRIE, BACKSTAGE_PASSES, SULFURAS } from '@/constants';
+
 export class Item {
 	name: string;
 	sellIn: number;
@@ -7,5 +9,51 @@ export class Item {
 		this.name = name;
 		this.sellIn = sellIn;
 		this.quality = quality;
+	}
+
+	update() {
+		if (this.name != AGED_BRIE && this.name != BACKSTAGE_PASSES) {
+			if (this.quality > 0) {
+				if (this.name != SULFURAS) {
+					this.quality = this.quality - 1;
+				}
+			}
+		} else {
+			if (this.quality < 50) {
+				this.quality = this.quality + 1;
+				if (this.name == BACKSTAGE_PASSES) {
+					if (this.sellIn < 11) {
+						if (this.quality < 50) {
+							this.quality = this.quality + 1;
+						}
+					}
+					if (this.sellIn < 6) {
+						if (this.quality < 50) {
+							this.quality = this.quality + 1;
+						}
+					}
+				}
+			}
+		}
+		if (this.name != SULFURAS) {
+			this.sellIn = this.sellIn - 1;
+		}
+		if (this.sellIn < 0) {
+			if (this.name != AGED_BRIE) {
+				if (this.name != BACKSTAGE_PASSES) {
+					if (this.quality > 0) {
+						if (this.name != SULFURAS) {
+							this.quality = this.quality - 1;
+						}
+					}
+				} else {
+					this.quality = this.quality - this.quality;
+				}
+			} else {
+				if (this.quality < 50) {
+					this.quality = this.quality + 1;
+				}
+			}
+		}
 	}
 }
