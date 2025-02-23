6dc684a78bf6cb856cfed8ca71e3b7b2dd20fd9d
Sander Maas <sandermaas7@hotmail.com>
2025-02-23 09:00:25 +0100

    Created tests for agedBrieStrategy

---
+++ app/item/strategies/aged-brie-strategy.spec.ts
@@ -0,0 +1,55 @@
+import { AGED_BRIE } from '../constants';
+import { Item } from '../Item';
+
+import { agedBrieStrategy } from './aged-brie-strategy';
+
+describe('agedBrieStrategy', () => {
+	describe('update', () => {
+		it('should decrease the sellIn by 1', () => {
+			const itemWithSellIn0 = new Item(AGED_BRIE, 0, 5);
+			const itemWithSellInNegative = new Item(AGED_BRIE, -4, 5);
+			const itemWithSellInPositive = new Item(AGED_BRIE, 7, 5);
+
+			agedBrieStrategy.update(itemWithSellIn0);
+			agedBrieStrategy.update(itemWithSellInNegative);
+			agedBrieStrategy.update(itemWithSellInPositive);
+
+			expect(itemWithSellIn0.sellIn).toBe(-1);
+			expect(itemWithSellInNegative.sellIn).toBe(-5);
+			expect(itemWithSellInPositive.sellIn).toBe(6);
+		});
+
+		it('should increase the quality by 1 when sellIn is higher than 0', () => {
+			const itemWithSellin3 = new Item(AGED_BRIE, 3, 12);
+			const itemWithSellin25 = new Item(AGED_BRIE, 25, 7);
+
+			agedBrieStrategy.update(itemWithSellin3);
+			agedBrieStrategy.update(itemWithSellin25);
+
+			expect(itemWithSellin3.quality).toBe(13);
+			expect(itemWithSellin25.quality).toBe(8);
+		});
+
+		it('should increase the quality by 2 when sellIn is 0 or less', () => {
+			const itemWithSellIn0 = new Item(AGED_BRIE, 0, 5);
+			const itemWithSellInNegative = new Item(AGED_BRIE, -8, 6);
+
+			agedBrieStrategy.update(itemWithSellIn0);
+			agedBrieStrategy.update(itemWithSellInNegative);
+
+			expect(itemWithSellIn0.quality).toBe(7);
+			expect(itemWithSellInNegative.quality).toBe(8);
+		});
+
+		it('should not increase the quality of an item over 50', () => {
+			const itemWithSellInNegative = new Item(AGED_BRIE, -3, 49);
+			const itemWithSellInPositive = new Item(AGED_BRIE, 6, 50);
+
+			agedBrieStrategy.update(itemWithSellInNegative);
+			agedBrieStrategy.update(itemWithSellInPositive);
+
+			expect(itemWithSellInNegative.quality).toBe(50);
+			expect(itemWithSellInPositive.quality).toBe(50);
+		});
+	});
+});
